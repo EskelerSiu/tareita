4e503248cb606baa2c93f753d1ebeb13eafdcd00
Angel Rafael Cruz Chavez <162520560+EskelerSiu@users.noreply.github.com>
2024-07-19 17:06:13 -0600

    yaaa

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,14 +1,21 @@
 import { Component, Input } from '@angular/core';
-import { NgOptimizedImage, CommonModule } from '@angular/common';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { Product } from './../../models/product.model';
 
 @Component({
   standalone: true,
-  imports: [NgOptimizedImage, CommonModule],
+  imports: [CommonModule],
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
   @Input() product!: Product;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  getSanitizedUrl(url: string): SafeUrl {
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
 }
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { NgOptimizedImage, CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { ProductComponent } from './../../components/product/product.component';
 import { Product } from './../../models/product.model';
 
@@ -11,15 +11,29 @@ import { Product } from './../../models/product.model';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
   http = inject(HttpClient);
   products: Product[] = [];
 
   ngOnInit() {
     this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
       .subscribe((data: Product[]) => {
-        this.products = data;
-        console.log(data);
+        this.products = data.map(product => {
+          // Limpia las URLs de las imágenes
+          product.images = product.images.map(image => {
+            if (typeof image === 'string' && image.startsWith('["')) {
+              try {
+                image = JSON.parse(image)[0];
+              } catch {
+                // Si no se puede parsear, deja la imagen tal como está
+                image = image.replace(/[\[\]"]+/g, ''); // Remueve cualquier comilla o corchete adicional
+              }
+            }
+            return image;
+          });
+          return product;
+        });
+        console.log(this.products);
       });
   }
 }
